b844a180b45236a21e08ec75879b2d428d2beb0b
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-04-09 11:30:33 +0300

    connect backend to database

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://harjoittelu_notes:${password}@cluster0.1cg5anc.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is Easy',
+  important: true,
+})
+
+const note_two = new Note({
+    content: "Browser can execute only JavaScript",
+    important: false,
+  })
+
+Note.find({important: true}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+  
+
+
+/*note.save().then(result => {
+  console.log('note saved!')
+  console.log(result)
+  mongoose.connection.close()
+})
+
+note_two.save().then(result => {
+    console.log('note saved!')
+    console.log(result)
+    mongoose.connection.close()
+  })*/
