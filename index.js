7e697f91acce4c97c4dbf69ae10ef373a8fda4bd
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-04-09 16:25:22 +0300

    uibuild

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
-  app.post('/api/notes', (request, response) => {
+  app.post('/api/notes', (request, response, next) => {
     const body = request.body
   
     if (body.content === undefined) {
@@ -69,17 +69,16 @@ app.delete('/api/notes/:id', (request, response, next) => {
     note.save().then(savedNote => {
       response.json(savedNote)
     })
+    .catch(error => next(error))
   })
 
   app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const { content, important } = request.body
   
-    const note = {
-      content: body.content,
-      important: body.important,
-    }
-  
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+      request.params.id,
+      { content, important },
+      { new: true, runValidators: true, context: 'query' }
       .then(updatedNote => {
         response.json(updatedNote)
       })
@@ -104,8 +103,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-
   next(error)
 }
  
