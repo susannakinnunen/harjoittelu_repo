c2020bc86d35b709315a54855ff10f0377287d55
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-04-09 14:10:00 +0300

    create and update notes with database

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const Note = require('./models/note')
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-
-app.use(express.static('build'))
- 
 
 let notes = [
     {
@@ -37,19 +35,25 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
-  
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
   app.post('/api/notes', (request, response) => {
     const body = request.body
   
@@ -67,7 +71,42 @@ app.get('/api/notes/:id', (request, response) => {
     })
   })
 
-  const PORT = process.env.PORT
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+ 
+app.use(errorHandler)
